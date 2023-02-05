3eaf396800688d3b521ada45e1c459b7fadd3757
zhenzhen <1967668484@qq.com>
2023-02-05 20:04:16 +0800

    完成黑马面经

--- hmmj/src/api/article.js
+++ hmmj/src/api/article.js
@@ -6,3 +6,36 @@ export const articleListAPI = params => {
     params
   })
 }
+// 封装一个添加面经的接口
+export const addArticleAPI = data => {
+  return request({
+    url: '/admin/interview/create',
+    method: 'post',
+    data
+  })
+}
+
+// 封装一个删除面经的接口
+export const delArticleAPI = data => {
+  return request({
+    url: '/admin/interview/remove',
+    method: 'delete',
+    data
+  })
+}
+// 封装一个获取面经详情的接口
+export const detailAPI = params => {
+  return request({
+    url: '/admin/interview/show',
+    method: 'get',
+    params
+  })
+}
+// 封装一个修改面经详情的接口
+export const updateAPI = data => {
+  return request({
+    url: '/admin/interview/update',
+    method: 'put',
+    data
+  })
+}
--- hmmj/src/views/article/index.vue
+++ hmmj/src/views/article/index.vue
@@ -33,16 +33,19 @@
         </el-table-column>
         <el-table-column label="操作" width="180">
           <template slot-scope="scope">
-            <el-link @click="showDrawer('view')"
+            <!-- 预览 -->
+            <el-link @click="showDrawer('view', scope.row.id)"
               ><i style="font-size: 18px" class="el-icon-view"></i
             ></el-link>
-            <el-link @click="showDrawer('edit')"
+            <!-- 编辑 -->
+            <el-link @click="showDrawer('edit', scope.row.id)"
               ><i
                 style="font-size: 18px;margin:0 10px;"
                 class="el-icon-edit-outline"
               ></i
             ></el-link>
-            <el-link @click="fn(scope)"
+            <!-- 删除 -->
+            <el-link @click="del(scope.row.id)"
               ><i
                 style="font-size: 18px;margin:0 10px;"
                 class="el-icon-delete"
@@ -66,24 +69,36 @@
       :visible.sync="drawer"
       direction="rtl"
       :size="600"
+      :before-close="beforeClose"
     >
+      <div class="article-preview" v-if="type === 'view'">
+        <h5>{{ form.stem }}</h5>
+        <div v-html="form.content"></div>
+      </div>
+
       <!-- 抽屉 -->
       <el-form
+        v-else
         :model="form"
         :rules="rules"
         ref="form"
         label-width="100px"
         class="demo-ruleForm"
       >
-        <el-form-item label="标题" prop="stem" v-model="form.name">
-          <el-input></el-input>
+        <el-form-item label="标题" prop="stem">
+          <el-input v-model="form.stem"></el-input>
         </el-form-item>
-        <el-form-item label="标题" prop="content" v-model="form.name">
-          <el-input></el-input>
+        <el-form-item label="内容" prop="content">
+          <!-- bidirectional data binding（双向数据绑定） -->
+          <quill-editor
+            v-model="form.content"
+            @blur="$refs.form.validateField('content')"
+          >
+          </quill-editor>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary">确认</el-button>
-          <el-button>取消</el-button>
+          <el-button type="primary" @click="save">确认</el-button>
+          <el-button @click="beforeClose(done)">取消</el-button>
         </el-form-item>
       </el-form>
     </el-drawer>
@@ -91,8 +106,24 @@
 </template>
 
 <script>
-import { articleListAPI } from '@/api/article'
+import {
+  articleListAPI,
+  addArticleAPI,
+  delArticleAPI,
+  detailAPI,
+  updateAPI
+} from '@/api/article'
+
+// require styles
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
+import { quillEditor } from 'vue-quill-editor'
 export default {
+  components: {
+    quillEditor
+  },
   name: 'article-page',
   data () {
     return {
@@ -104,8 +135,7 @@ export default {
       type: 'add',
       form: {
         stem: '',
-        content: '',
-        name: ''
+        content: ''
       },
       rules: {
         stem: [{ required: true, message: '请输入标题', trigger: 'blur' }],
@@ -118,6 +148,20 @@ export default {
     this.loadData()
   },
   methods: {
+    beforeClose (done) {
+      // console.log('关闭了')
+      // this.$refs.form.resetFields()
+      this.form = {
+        stem: '',
+        content: ''
+      }
+      if (this.type !== 'view') {
+        this.$refs.form.clearValidate(['stem', 'content'])
+      }
+
+      this.drawer = false
+      done()
+    },
     async loadData () {
       const res = await articleListAPI({
         current: this.page,
@@ -133,10 +177,60 @@ export default {
       this.loadData()
     },
 
-    showDrawer (type) {
-      console.log(type)
-      this.drawer = true
+    async showDrawer (type, id) {
+      // 先发请求再显示，新增不需要再发送请求。
+      if (type !== 'add') {
+        const res = await detailAPI({ id })
+        console.log('llll', res)
+        this.form = res.data
+      }
+
       this.type = type
+      this.drawer = true
+    },
+    // 点击确认触发的事件。
+    async save (id) {
+      try {
+        // 整个表单校验
+        await this.$refs.form.validate()
+        if (this.type === 'add') {
+          // 能来到这，证明校验通过，就发起请求做新增
+          const res = await addArticleAPI(this.form)
+          console.log(res)
+          // 弹出成功提示
+          this.$message.success('新增成功')
+
+          // 跳转到第一个
+          this.page = 1
+        } else {
+          // 要么是新增要么是修改，预览只是给你来看的
+          await updateAPI({
+            id: this.form.id,
+            content: this.form.content,
+            stem: this.form.stem
+          })
+          this.$message.success('修改成功')
+        }
+
+        // 渲染数据
+        this.loadData()
+        // 关闭抽屉
+        // this.drawer = false
+        this.beforeClose()
+      } catch {}
+    },
+    // 删除面经
+    async del (id) {
+      const res = await delArticleAPI({
+        id
+      })
+      console.log(res)
+      // 弹出提示
+      this.$message.success('删除成功')
+      if (this.tableData.length === 1 && this.page !== 1) {
+        this.page--
+      }
+      this.loadData()
     }
   },
   computed: {
